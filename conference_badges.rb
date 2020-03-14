acca9f411c710e6d29a052089de798bd9e961449
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-14 22:36:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(room_assignments)
   room_assignments.each_with_index do |room_assignments, index|
     empty_rooms << "Hello, #{room_assignments}! You'll be assigned to room #{index + 1}!"
   end
-  return empty_rooms
+  empty_rooms
 end
 
 def printer(attendees)
