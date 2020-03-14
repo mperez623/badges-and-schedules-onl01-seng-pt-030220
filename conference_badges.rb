01271f7da8fafe45b71e371d0604baffea3e3376
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-14 21:52:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(attendees)
   names = []
   array.each do |name|
     names << badge_maker(array) 
