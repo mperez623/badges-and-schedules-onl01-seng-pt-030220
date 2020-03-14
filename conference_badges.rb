e4fd08d78f772e03fbac9e4a4690ada12f86bc25
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-14 22:29:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(room_assignments)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each #{|x| puts x} 
-  assign_room(attendees).each #{|y| puts y}
+  batch_badge_creator(attendees).each {|x| puts x} 
+  assign_room(attendees).each {|y| puts y}
 end
   
